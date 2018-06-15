7eede20a2a654bb7ef20f34e1007bfb9e367cfff
rpinto <rachel.lea.pinto@gmail.com>
2018-06-15 13:27:26 -0700

    Added function stubs and user stories, created main function

---
+++ index.js
@@ -0,0 +1,34 @@
+/*
+As a user, upon first loading the app, I can see an empty shopping list
+As a user, I can enter items I need to purchase by entering text and hitting "Return" or clicking the "Add item" button
+As a user, I can delete an item from the list
+As a user, check and uncheck items on the list by clicking the "Check" button
+As a user, I can press a switch/checkbox to toggle between displaying all items or displaying only items that are unchecked
+As a user, I can type in a search term and the displayed list will be filtered by item names only containing that search term
+As a user, I can edit the title of an item
+*/
+
+//Make function stubs
+
+function renderShoppingList() {}
+function addItem() {}
+function deleteItem() {}
+function checkItemComplete() {}
+function toggleItems() {}
+function filterBySearch() {}
+function editItemTitle() {}
+
+function main() {
+
+    function renderShoppingList() {}
+    function addItem() {}
+    function deleteItem() {}
+    function checkItemComplete() {}
+    function toggleItems() {}
+    function filterBySearch() {}
+    function editItemTitle() {}
+}
+
+// Run the `main` function when DOM loads
+$(main);
+
